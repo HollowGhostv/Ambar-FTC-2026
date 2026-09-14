07b3d5529fa644ab2163042a25baf88a65c92833
migue <miguelangelmadrizcortez@gmail.com>
2026-09-14 17:48:15 -0600

    nombre

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_TeleOp.java
@@ -12,6 +12,7 @@ public class FTC_TeleOp {
         //Agreguen comentario con su nombre los que ya tengan acceso, abajo de este comentario
         //André Maximiliano Celis Ruiz
         //Alexis Santiago Quiles Perez
+        //Miguel Angel Madriz Cortez
     }
 
     void loop()
