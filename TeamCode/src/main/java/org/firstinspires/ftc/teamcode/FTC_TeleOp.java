e43755cb3dad227e6c456315abe6b252374c2d51
Adolfo <adolfomoranosuna@gmail.com>
2026-09-14 17:14:08 -0600

    Actualizacion codigo TeleOp.v2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_TeleOp.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+
+@TeleOp
+public class FTC_TeleOp {
+
+    void init()
+    {
+        //Agreguen comentario con su nombre los que ya tengan acceso, abajo de este comentario
+    }
+
+    void loop()
+    {
+
+    }
+}
